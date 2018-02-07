ceb2eadcfa72c828ccaae9a14b6b1304b15a826a
Lucian Henrique dos Santos Caetano <lucian.santos.caetano@gmail.com>
2018-02-07 15:36:53 -0200

    Update showif-hideif.stories.js

--- stories/showif-hideif.stories.js
+++ stories/showif-hideif.stories.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { ShowIF, HideIF } from '../src/index';
+import { ShowIf, HideIf } from '../src/index';
 import { withKnobs, boolean } from '@storybook/addon-knobs/react';
 import { storiesOf, setAddon } from '@storybook/react';
 import JSXAddon from 'storybook-addon-jsx';
 
 setAddon(JSXAddon);
 
-let stories = storiesOf('ShowIF and HideIf', module);
+let stories = storiesOf('ShowIf and HideIf', module);
 stories.addDecorator(withKnobs);
 
-stories.addWithJSX('ShowIF', () => (
+stories.addWithJSX('ShowIf', () => (
     <ShowIF value={boolean('Bool Value', true)}>
         <button>Hello World</button>
     </ShowIF>
 ));
 
-stories.addWithJSX('HideIF', () => (
+stories.addWithJSX('HideIf', () => (
     <HideIF value={boolean('Bool Value', true)}>
         <button>Hello World</button>
     </HideIF>
